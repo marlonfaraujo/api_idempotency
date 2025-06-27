f2c496b53d4ed8e86cc7993fc342d2d02dbf424d
Marlon <marlonfda@gmail.com>
2025-06-27 00:27:15 -0300

    Adding shell script to create initial database

---
+++ postgres-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Running in container postgres..."
+
+# Drop database
+echo "Droping database..."
+docker exec -i idempotency_api_postgres psql -U postgres -d postgres -c "drop database if exists developer;"
+
+# Create database
+echo "Creating database..."
+docker exec -i idempotency_api_postgres psql -U postgres -d postgres -c "create database developer;"
+
+# Run script.sql
+echo "Running script.sql ..."
+docker exec -i idempotency_api_postgres psql -U postgres -d developer < ./script.sql 
+
+echo "Done."
+
+#command run: bash ./postgres-docker.sh
